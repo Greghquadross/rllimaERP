ea92fc0a147796e228c222caadbd796f9f1cb696
Gregory de Quadros <greghquadross@gmail.com>
2024-10-10 21:50:28 -0300

    #

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('produtos')->group(function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
